9ecfc32286c488bd36cc2843295bbf281f33f09d
Daniel Coupal <dacoupal@gmail.com>
2024-05-15 12:24:52 -0400

    Generate C modules

---
+++ CModuleGen.psm1
@@ -0,0 +1,145 @@
+################################################################################
+#!/usr/bin/pwsh
+#
+# Date: 2022-04-25
+# Last: 2023-03-09
+# Author: Daniel Coupal
+# Create C module header, source and -optionally- test file(s).
+#
+# Include copy of ./proj_conf.ps1 with project-specific details.
+# For best results, use Doxygen module name as "-ModuleName" arg.
+################################################################################
+
+New-Variable -Name ProjName -Scope Script -Force
+New-Variable -Name Year -Scope Script -Force
+New-Variable -Name Date -Scope Script -Force
+New-Variable -Name Compiler -Scope Script -Force
+New-Variable -Name Target -Scope Script -Force
+New-Variable -Name Author -Scope Script -Force
+
+function New-Header {
+    param (
+        [string]$fileBaseName
+    )
+
+    $boiler = Get-Content "$env:REPOS/CModuleGen/header.templ"
+    $boiler = $boiler -Replace '{FileBaseName}', "$fileBaseName"
+    $boiler = $boiler -Replace '{ProjName}', "$ProjName"
+    $boiler = $boiler -Replace '{Year}', "$Year"
+    $boiler = $boiler -Replace '{Date}', "$Date"
+    $boiler = $boiler -Replace '{DoxGroup}', "$fileBaseName"
+    $boiler = $boiler -Replace '{DoxGroupName}', "$doxGroupName"
+    $boiler = $boiler -Replace '{Author}', "$Author"
+    $boiler = $boiler -Replace '{INCLUDE_GUARD}', "$includeGuard"
+    if ($WithVersion) {
+        $boiler = $boiler -Replace '{Version}', " * @version 0.1.0`n *`n"
+    }
+    else {
+        $boiler = $boiler | Select-String '{Version}' -NotMatch
+    }
+
+    if ($PrintOnly -or $Debug) {
+        Write-Output $boiler
+    }
+    else {
+        $boiler | Tee-Object "$fileBaseName.h"
+    }
+}
+
+function New-Source {
+    param (
+        [string]$fileBaseName
+    )
+
+    $boiler = Get-Content "$env:REPOS/CModuleGen/source.templ"
+    $boiler = $boiler -Replace '{FileBaseName}', "$fileBaseName"
+    $boiler = $boiler -Replace '{ProjName}', "$ProjName"
+    $boiler = $boiler -Replace '{Year}', "$Year"
+    $boiler = $boiler -Replace '{Date}', "$Date"
+    $boiler = $boiler -Replace '{Compiler}', "$Compiler"
+    $boiler = $boiler -Replace '{Target}', "$Target"
+    $boiler = $boiler -Replace '{DoxGroup}', "$fileBaseName"
+    $boiler = $boiler -Replace '{Author}', "$Author"
+
+    if ($RTOS) {
+        $boiler = $boiler -Replace '{RTOS}', "
+  /*==============================================================================
+   = OS ROUTINES
+   =============================================================================*/
+  "
+    }
+    else {
+        $boiler = $boiler | Select-String '{RTOS}' -NotMatch
+    }
+
+    if ($PrintOnly -or $Debug) {
+        Write-Output $boiler
+    }
+    else {
+        $boiler | Tee-Object "$fileBaseName.c"
+    }
+}
+
+function New-Unit-Test {
+    param (
+        [string]$fileBaseName
+    )
+
+    $boiler = Get-Content "$env:REPOS/CModuleGen/unit_test.templ"
+    $boiler = $boiler -Replace '{FileBaseName}', "$fileBaseName"
+    $boiler = $boiler -Replace '{ProjName}', "$ProjName"
+    $boiler = $boiler -Replace '{Year}', "$Year"
+    $boiler = $boiler -Replace '{Date}', "$Date"
+    $boiler = $boiler -Replace '{Compiler}', "$Compiler"
+    $boiler = $boiler -Replace '{Target}', "$Target"
+
+    if ($PrintOnly -or $Debug) {
+        Write-Output $boiler
+    }
+    else {
+        $boiler | Tee-Object "test_$fileBaseName.c"
+    }
+}
+
+function New-CModule {
+    param (
+        [Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true)]
+        [string]$ModuleName,
+        [switch]$WithVersion,
+        [switch]$RTOS,
+        [switch]$UnitTest,
+        [switch]$PrintOnly,
+        [Parameter()] 
+        [ValidateSet("Header", "Source", "UnitTest")] 
+        [string[]]$Only = "All"
+    )
+
+    if ($ModuleName.Length -eq 0) {
+        Write-Error "No -ModuleName. Exiting." 
+        return
+    }
+
+    $fileBaseName = $ModuleName.ToLower() -Replace " ", "_"
+    $doxGroupName = $ModuleName -Replace "_", " "
+    $includeGuard = ($ModuleName.ToUpper() -Replace " ", "_") + "_H_"
+
+    if (Test-Path "./proj_conf.ps1") {
+        $ProjName, $Year, $Date, $Compiler, $Target, $Author = ./proj_conf.ps1
+    }
+    else {
+        $ProjName, $Year, $Date, $Compiler, $Target, $Author = Read-Host
+    }
+
+    if($Only -in ("All", "Header")) {
+        New-Header $fileBaseName
+    }
+    if($Only -in ("All", "Source")) {
+        New-Source $fileBaseName
+    }
+    if(($Only -like "All" -and $UnitTest) -or $Only -like "UnitTest") {
+        New-Unit-Test $fileBaseName
+    }
+}
+
+Export-ModuleMember -Function 'New-CModule'
+
---
+++ proj_conf.ps1
@@ -0,0 +1,10 @@
+
+$ProjName       = "My proj"
+$Year           = "My yr"
+$Date           = Get-Date -Format 'yyyy-MM-dd'
+$Compiler       = "My comp"
+$Target         = "My targ"
+$Author         = "My author"
+
+$ProjName, $Year, $Date, $Compiler, $Target, $Author
+
